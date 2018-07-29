6d1ed4d6307626e92d60e0a27fc600f013bbdaa2
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2018-07-29 10:17:04 +0900

    Implement first version

--- src/main.ino
+++ src/main.ino
@@ -2,92 +2,85 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
-#include "BluetoothSerial.h"
+#include "Arduino.h"
+#include "Version.h"
 #include "Log.h"
-#include "CommandLine.h"
-#include "InfoCommand.h"
-#include "LogCommand.h"
-#include "GetButtonStateCommand.h"
-#include "SetLogLevelCommand.h"
-#include "SetDigitalCommand.h"
-#include "SetServoCommand.h"
-#include "DriveSteeringCommand.h"
-#include "SetMotorCommand.h"
-#include "DriveMotorCommand.h"
-#include "GetDistanceCommand.h"
+#include "Wire.h"
+#include "Servo.h"
 
 enum {
-    kDebugLed = 26,
+    kRv8830Control  = 0,
+    kRv8830Address  = 100,
+    kI2cSda         = 21,
+    kI2cScl         = 22,
+    kServo          = 12,
+    kDebugLed       = 26,
 };
 
-BluetoothSerial stream;
+int DriveMotor(int control) {
+    Wire.beginTransmission(kRv8830Address);
+    Wire.write(kRv8830Control);
+    Wire.write(control);
 
-CommandLine           commandLine;
-InfoCommand           infoCommand;
-LogCommand            logCommand;
-GetButtonStateCommand getButtonStateCommand;
-SetLogLevelCommand    setLogLevelCommand;
-SetDigitalCommand     setDigitalCommand;
-SetServoCommand       setServoCommand;
-DriveSteeringCommand  driveSteeringCommand;
-SetMotorCommand       setMotorCommand;
-DriveMotorCommand     driveMotorCommand;
-GetDistanceCommand    getDistanceCommand;
+    int result = Wire.endTransmission();
+    switch (result) {
+    case 0: Log::Trace("set_motor: success");                         return 0;
+    case 1: Log::Error("set_motor: buffer overflow error");           return -1;
+    case 2: Log::Error("set_motor: slave addr: nack received error"); return -2;
+    case 3: Log::Error("set_motor: data: nack received error");       return -3;
+    case 4: Log::Error("set_motor: error");                           return -4;
+    }
+    // workaround for esp-32 i2c bug
+    // see http://d.hatena.ne.jp/wakwak_koba/20171228
+    // see https://github.com/espressif/arduino-esp32/issues/349
+    // Wire.reset();
+    Log::Error("set_motor: unknown result error(workaround for esp-32 i2c bug)");
+    return 3;
+}
+
+int DriveServo(int degrees) {
+    Servo servo;
+    servo.attach(kServo);
+    servo.write(degrees);
+    vTaskDelay(200);
+    servo.detach();
+    return 0;
+}
 
 void setup()
 {
     pinMode(kDebugLed, OUTPUT);
     digitalWrite(kDebugLed, HIGH);
+
+    pinMode(kI2cSda, INPUT_PULLUP);
+    pinMode(kI2cScl, INPUT_PULLUP);
+    Wire.begin();
+    Wire.setClock(10000);
+
     Serial.begin(115200);
-    stream.begin("Quattro Ace");
-
-    commandLine.Initialize(&stream);
-
-    infoCommand.Initialize(&stream);
-    commandLine.AddCommand(&infoCommand);
-
-    logCommand.Initialize(&stream);
-    commandLine.AddCommand(&logCommand);
-
-    getButtonStateCommand.Initialize(&stream);
-    commandLine.AddCommand(&getButtonStateCommand);
-
-    commandLine.AddCommand(&setLogLevelCommand);
-    commandLine.AddCommand(&setDigitalCommand);
-    commandLine.AddCommand(&setServoCommand);
-
-    driveSteeringCommand.Initialize(&setServoCommand);
-    commandLine.AddCommand(&driveSteeringCommand);
-
-    setMotorCommand.Initialize(&stream);
-    commandLine.AddCommand(&setMotorCommand);
-
-    driveMotorCommand.Initialize(&stream,
-                                 &setMotorCommand,
-                                 &getDistanceCommand,
-                                 &getButtonStateCommand);
-    commandLine.AddCommand(&driveMotorCommand);
-
-    commandLine.AddCommand(&getDistanceCommand);
 
     Log::Info("Quattro Ace started...");
+    Log::Info(VERSION);
     digitalWrite(kDebugLed, LOW);
 }
 
 void loop()
 {
-    digitalWrite(kDebugLed, LOW);
-    commandLine.Analyze();
+    // go forward
+    DriveMotor(0x61);
+    vTaskDelay(3000);
 
-    static int executeTimes = 0;
-    if (executeTimes > 100) {
-        executeTimes = 0;
-    } else if (executeTimes > 50) {
-        digitalWrite(kDebugLed, HIGH);
-    } else {
-        digitalWrite(kDebugLed, LOW);
-    }
-    ++executeTimes;
+    // stop
+    DriveMotor(0);
+    vTaskDelay(1000);
+
+    // go backward
+    DriveMotor(0x61);
+    vTaskDelay(3000);
+
+    // stop
+    DriveMotor(0);
+    vTaskDelay(1000);
 
     // Task watchdog got triggered.
     // https://github.com/espressif/arduino-esp32/issues/595
